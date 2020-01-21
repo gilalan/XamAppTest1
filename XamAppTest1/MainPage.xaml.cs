b6358609cd576c5bf68daa2595f1219c678e80c0
Gilliard Lopes <gilalan@gmail.com>
2020-01-20 22:42:13 -0300

    Adding navigation

--- XamAppTest1/MainPage.xaml.cs
+++ XamAppTest1/MainPage.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using XamAppTest1.Models;
+using XamAppTest1.Services;
 using XamAppTest1.ViewModels;
+using XamAppTest1.Views;
 using Xamarin.Forms;
 
 namespace XamAppTest1
@@ -9,10 +14,30 @@ namespace XamAppTest1
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        SoccerApiService _service = new SoccerApiService();
         public MainPage()
         {
             InitializeComponent();
             this.BindingContext = new MainViewModel();
+        }
+
+        private async void BtnLogin_Clicked(object sender, System.EventArgs e)
+        {
+            //var response = await _service.GetJobsAsync();
+            //if (string.IsNullOrEmpty(response))
+            //{
+
+            //}
+            List<Job> jobsResponse = await _service.GetJobsAsync();
+            if (jobsResponse.Count > 0)
+            {
+                Application.Current.MainPage = new NavigationPage(new HomePage());
+                //await Navigation.PushAsync(new HomePage());
+            } 
+            else
+            {
+                await DisplayAlert("Alert", "Não tinha Jobs", "OK");
+            }
         }
     }
 }
--- XamAppTest1/Services/SoccerApiService.cs
+++ XamAppTest1/Services/SoccerApiService.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,7 +13,28 @@ namespace XamAppTest1.Services
 {
     public class SoccerApiService
     {
-        
+        public async Task<User> GetUserAsync()
+        {
+            User user = new User();
+            try
+            {
+                var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = await httpClient.GetAsync(Constants.ApiBaseUrl + "login");
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = response.Content.ReadAsStringAsync().Result;
+                    user = JsonConvert.DeserializeObject<User>(content);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            return user;
+        }
         
 		public async Task<List<Job>> GetJobsAsync()
 		{
@@ -27,20 +49,38 @@ namespace XamAppTest1.Services
 				var response = await httpClient.GetAsync(Constants.ApiBaseUrl + "cargos");
 				if (response.IsSuccessStatusCode)
 				{
-					string content = response.Content.ReadAsStringAsync().Result;
-					//dynamic result = JsonConvert.DeserializeObject(content);
-					//var job = new Job();
-					//job.Specification = result.data.results[0].especificacao;
-					//job.FemaleName = result.data.results[0].nomeFeminino;
-					//job.Description = result.data.results[0].descricao;
-					//job.CBO = result.data.results[0].cbo;
-					//jobs.Add(job);
-					var Items = JsonConvert.DeserializeObject<List<Job>>(content);
-					foreach (var item in Items)
-					{
-						Debug.WriteLine(item.Specification);
-					}
-				}
+                    using(HttpContent content = response.Content)
+                    {
+                        var json = content.ReadAsStringAsync();
+                        //jobs = JsonConvert.DeserializeObject<List<Job>>(json.Result);
+                        //foreach (Job job in jobs)
+                        //{
+                        //    Debug.WriteLine(job.CBO);
+                        //}
+                        //dynamic result = JsonConvert.DeserializeObject(json.Result);
+                        JArray resultArray = (JArray) JsonConvert.DeserializeObject(json.Result);
+                        var job = new Job();
+                        foreach (JObject item in resultArray)
+                        {
+                            job = new Job();
+                            job.CBO = (item.GetValue("cbo") != null) ? item.GetValue("cbo").Value<string>() : "";
+                            job.Description = (item.GetValue("descricao") != null) ? item.GetValue("descricao").Value<string>() : "";
+                            job.FemaleName = (item.GetValue("nomeFeminino") != null) ? item.GetValue("nomeFeminino").Value<string>() : "";
+                            job.Specification = item.GetValue("especificacao") != null ? item.GetValue("especificacao").Value<string>() : "";
+                            jobs.Add(job);
+                        }
+                        //foreach (dynamic item in result.data.results)
+                        //{
+                        //    job.Specification = item.especificacao;
+                        //    job.FemaleName = item.nomeFeminino;
+                        //    job.Description = item.descricao;
+                        //    job.CBO = item.cbo;
+                        //    jobs.Add(job);
+                        //}
+
+                    }
+
+                }
 				
 			}
 			catch (Exception ex)
